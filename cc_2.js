74830e44acafd51d1cd5e6d58bb63a765a0cc3b1
Jordan Higgins <jordanhiggins@usf.edu>
2025-01-26 17:54:28 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -8,3 +8,14 @@ products.push("Earmuffs");
 products.pop();
 // Prints the list of the current inventory.
 console.log("Current Inventory:",products);
+
+// Task 2: Student Scores
+// Created an array of student scores.
+let scores = [95,78,84,45,62];
+// Edits the second score entry.
+scores[1] = 75;
+// Calculates the average between the scores.
+let average = (scores[0] + scores[1] + scores[2] + scores[3] + scores[4])/scores.length;
+// Posts the Updated and Average scores to the console.
+console.log("Updated Scores:", scores);
+console.log("Average Scores:", average);

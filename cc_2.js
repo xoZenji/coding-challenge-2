972954c9a2bafc1cf20f32cfd7709f5d9492034a
Jordan Higgins <jordanhiggins@usf.edu>
2025-01-26 17:26:37 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,10 @@
+// Task 1: Store Inventory
+
+// Creates the array of products.
+let products = ["Beanie","Hoodie","Gloves","Sweatpants","Longsleeve"];
+// Pushed a new item onto the end of the list.
+products.push("Earmuffs"); 
+// Removes the last item on the list.
+products.pop();
+// Prints the list of the current inventory.
+console.log("Current Inventory:",products);
